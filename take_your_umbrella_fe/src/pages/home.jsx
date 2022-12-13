b3e65e1d22708a731551cd054e27e64d85c591bf
zaticpetru <zatic.petru@outlook.com>
2022-12-13 19:15:51 +0200

    FE create react app create home page create error page setup react router

---
+++ take_your_umbrella_fe/src/pages/home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <img id="logo-image" src="/images/umbrella.png" alt="umbrella logo"/>
+            <h1 id="title">Take your umbrella</h1>
+            <button id="start-button">START</button>
+        </>
+    )
+}
